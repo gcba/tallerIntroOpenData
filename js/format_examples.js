16c26d03ad208e9e69651d4e5d2ce1a5655fb417
Ignacio Heredia <ignacio.nh@gmail.com>
2015-10-27 16:34:51 -0300

    Mostrando ejemplos de json

--- js/format_examples.js
+++ js/format_examples.js
@@ -1,10 +1,17 @@
 $(function() {
     $.ajax({
         url:'./data/example.xml',
-        datatype:'text',
         success: function(response) {
             var xml_string = new XMLSerializer().serializeToString(response);
             $('code.xml').text(xml_string);
+        },
+        async: false
+    });
+    $.ajax({
+        url:'./data/example.json',
+        dataType:'text',
+        success: function(response) {
+            $('code.json').text(response);
         },
         async: false
     });
