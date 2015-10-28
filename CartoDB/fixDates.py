605825132a7356f6676ecc82a07d89e1f7dc7802
josesalgado1024 <jsalgado@jsalgado-VPCEA23EL.(none)>
2015-10-28 11:55:49 -0300

    .

--- CartoDB/fixDates.py
+++ CartoDB/fixDates.py
@@ -8,7 +8,7 @@ if len(sys.argv) > 1:
 c = 0
 with open(filename, 'r') as csv_file:
     csv_rows = csv.reader(csv_file, delimiter=';')
-    ncsv = '%s_fixed.%s' % (filename.split('.')[0], filename.split('.')[1])
+    ncsv = '%s_timestamp.%s' % (filename.split('.')[0], filename.split('.')[1])
     with open(ncsv, 'w') as fixCSV:
         for row in csv_rows:
             time = row[5].split(' ')
@@ -22,11 +22,11 @@ with open(filename, 'r') as csv_file:
                 else:
                     hour, mins = date.split(':')
                     sec = '00'
-                time = '%s/%s/%s, %s:%s:%s' % (year, month, day, hour, mins, sec)
+                time = '%s/%s/%sT%s:%s:%sZ' % (year,  month, day, hour, mins, sec)
                 stdscr.addstr(0, 0, '>> %s ' % time)
                 stdscr.refresh()
                 c += 1
             else:
-                time = 'fecha, hora'
+                time = 'timestamp'
             fixCSV.write('%s,%s,%s,%s,%s,%s,%s,%s\n' % (row[0], row[1], row[2], row[3], row[4], time, row[6], row[7]))
             fixCSV.flush()
